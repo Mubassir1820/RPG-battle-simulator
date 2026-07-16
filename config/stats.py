83c7767889574ae2efd1f122a0a798c339be4c72
mubassir1820 <mubassir.habib@northsouth.edu>
2026-07-16 19:52:00 +0600

    project structure implementation

---
+++ config/stats.py
@@ -0,0 +1,20 @@
+CHARACTER_STATS = {
+    "Warrior": {
+        "health": 150,
+        "attack": 25,
+        "defense": 20,
+        "mana": 20,
+    },
+    "Mage": {
+        "health": 90,
+        "attack": 15,
+        "defense": 8,
+        "mana": 100,
+    },
+    "Archer": {
+        "health": 110,
+        "attack": 20,
+        "defense": 12,
+        "mana": 40,
+    },
+}
